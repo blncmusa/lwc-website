af42850f16209d4c799edba10e696f0c9c26c3c9
Musa <97222228+blncmusa@users.noreply.github.com>
2024-10-12 21:38:30 +0100

    fixing gh-pages, build outDir

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: '/lwc-website/',
+  build: {
+    outDir: 'docs'  
+  },
   css: {
     postcss: './postcss.config.js',
   },
