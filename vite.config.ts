5f3a7ee7d078c6a0e658388adb714d6667f74948
Musa <97222228+blncmusa@users.noreply.github.com>
2024-10-13 05:05:39 +0100

    rebuilt dist.

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base: mode === 'production' ? '/lwc-website/' : '/',
-    build: {
-      outDir: 'dist'
-    },
-    css: {
-      postcss: './postcss.config.js',
-    }
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: '/',  // This keeps the base URL as root for production and local development.
+  build: {
+    outDir: 'dist',  // Output directory for production builds.
+    // Optionally, if you're planning to include a source map for debugging:
+    sourcemap: true, 
+  },
+  css: {
+    postcss: './postcss.config.js',
+  }
 });
