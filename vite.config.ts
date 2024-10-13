b651bcd9e33ac862568c9531d402630f29702639
Musa <97222228+blncmusa@users.noreply.github.com>
2024-10-13 04:51:07 +0100

    fix:vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/lwc-website/',
-  build: {
-    outDir: 'docs'  
-  },
-  css: {
-    postcss: './postcss.config.js',
-  },
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/lwc-website/' : '/',
+    build: {
+      outDir: 'dist'
+    },
+    css: {
+      postcss: './postcss.config.js',
+    }
+  };
+});
